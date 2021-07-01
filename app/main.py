404c4105088bcc1183dd2584761c162c1cf7fdd9
Anthony Maladot <anthony.maladot@netc.fr>
2021-07-01 23:37:48 +0200

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from typing import Optional,List
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api_client import ApiClient
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api_client = ApiClient()
 
