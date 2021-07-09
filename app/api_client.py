9c44192405270bd7b129519474c2b910d2b6a27e
Anthony Maladot <anthony.maladot@netc.fr>
2021-07-09 22:25:31 +0200

    download image as base64

--- app/api_client.py
+++ app/api_client.py
@@ -1,18 +1,21 @@
 import aiohttp
+import base64
 import pydash
 
 class ApiClient():
     def __init__(self):
         self.base_url = 'http://www.hofc.fr/wp-json'
 
-    def _map_actus(self, wp_actus):
-        return [{
-            "id": wp_actu['id'],
-            "titre": wp_actu['title']['rendered'],
-            "contenu": wp_actu['content']['rendered'],
-            "date_creation": wp_actu['date_gmt']+"Z",
-            "image_url": pydash.get(wp_actu, '_embedded.wp:featuredmedia.0.source_url', None)
-        } for wp_actu in wp_actus]
+    async def _map_actus(self, wp_actus):
+        async with aiohttp.ClientSession() as session:
+            return [{
+                "id": wp_actu['id'],
+                "titre": wp_actu['title']['rendered'],
+                "contenu": wp_actu['content']['rendered'],
+                "date_creation": wp_actu['date_gmt']+"Z",
+                "image_url": pydash.get(wp_actu, '_embedded.wp:featuredmedia.0.source_url', None),
+                "image_base64": base64.b64encode(await (await session.get(pydash.get(wp_actu, '_embedded.wp:featuredmedia.0.source_url', None))).read())
+            } for wp_actu in wp_actus]
 
     async def get_actus(self):
         async with aiohttp.ClientSession() as session:
@@ -21,4 +24,4 @@ class ApiClient():
                 print("Status:", response.status)
                 print("Content-type:", response.headers['content-type'])
 
-                return self._map_actus(await response.json())
+                return await self._map_actus(await response.json())
